509369502fcc68e24e3f956eae7541c756cad51a
deepbiolab <lin_xiangtian@outlook.com>
2025-06-03 20:46:55 +0800

    update

--- src/model.py
+++ src/model.py
@@ -1,5 +1,12 @@
 """
-Contrastive Language Image Pretraining
+Contrastive Language Image Pretraining with Sigmoid Loss.
+
+- Unlike standard contrastive learning with softmax normalization, the sigmoid loss
+operates solely on image-text pairs and does not require a global view of the
+pairwise similarities for normalization.
+
+- The sigmoid loss simultaneously allows further scaling up the batch size,
+while also performing better at smaller batch sizes
 
 References:
 0. Transformer: https://arxiv.org/pdf/1706.03762
@@ -12,6 +19,7 @@ from dataclasses import dataclass
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from einops import rearrange
 
 
 @dataclass
@@ -33,6 +41,139 @@ class VisionConfig:
 
     # Vit output
     num_image_tokens: int = None
+
+
+class MHAttention(nn.Module):
+    """Multi-Head Attention Layer.
+
+    Reference:
+        Transformer: https://arxiv.org/pdf/1706.03762
+    """
+
+    def __init__(self, config: VisionConfig):
+        super().__init__()
+        self.config = config
+        self.embed_dim = config.hidden_size
+        self.num_heads = config.num_attention_heads
+        self.head_dim = self.embed_dim // self.num_heads
+        self.scale = self.head_dim**-0.5
+        self.dropout = config.attention_dropout
+
+        self.qkv_proj = nn.Linear(self.embed_dim, self.embed_dim * 3)
+        self.out_proj = nn.Linear(self.embed_dim, self.embed_dim)
+
+    def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
+        """Multi-Head Attention"""
+        # get q, k, v tuple
+        qkv = self.qkv_proj(hidden_states).chunk(3, dim=-1)
+
+        # reshape each by spliting embed dim into multiple heads
+        q, k, v = map(
+            lambda t: rearrange(t, "b n (h d) -> b h n d", h=self.num_heads), qkv
+        )
+
+        # calculate similarities
+        sim = torch.einsum("b h i d, b h j d -> b h i j", q, k) * self.scale
+
+        # get attention matrix
+        attn = torch.softmax(sim, dim=-1)
+
+        # weigh the v matrix based on attention matrix
+        out = torch.einsum("b h i j, b h j d -> b h i d", attn, v)
+
+        # reshape it into original shape: batch_size, seq_len, embed_dim
+        out = rearrange(out, "b h n d -> b n (h d)")
+
+        # project output
+        out = self.out_proj(out)
+
+        return out, attn
+
+
+class MLP(nn.Module):
+    """Feed Forward Layer of Transformer Encoder.
+
+    Reference:
+        Transformer: https://arxiv.org/pdf/1706.03762
+    """
+
+    def __init__(self, config: VisionConfig):
+        super().__init__()
+        self.config = config
+        self.fc1 = nn.Linear(config.hidden_size, config.feedforward_size)
+        self.fc2 = nn.Linear(config.feedforward_size, config.hidden_size)
+
+    def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
+        """Position-wise fully connected feed-forward network"""
+        # [batch_size, num_patches, embed_size]
+        hidden_states = self.fc1(hidden_states)
+        # [batch_size, num_patches, embed_size * 4]
+        hidden_states = F.gelu(hidden_states, approximate="tanh")
+        # [batch_size, num_patches, embed_size]
+        hidden_states = self.fc2(hidden_states)
+        return hidden_states
+
+
+class EncoderLayer(nn.Module):
+    """Encoder layer of transformer.
+
+    Reference:
+        Vit: https://arxiv.org/pdf/2010.11929
+    """
+
+    def __init__(self, config: VisionConfig):
+        super().__init__()
+        self.embed_dim = config.hidden_size
+        self.layer_norm1 = nn.LayerNorm(self.embed_dim, eps=config.layer_norm_eps)
+        self.mha = MHAttention(config)
+        self.layer_norm2 = nn.LayerNorm(self.embed_dim, eps=config.layer_norm_eps)
+        self.mlp = MLP(config)
+
+    def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
+        """Standard transformer encoder layer"""
+        # [batch_size, num_patches, embed_size]
+        residual = hidden_states
+
+        # Norm
+        hidden_states = self.layer_norm1(hidden_states)
+        # Multi-Head Attention
+        hidden_states, _ = self.mha(hidden_states)
+
+        # Add
+        hidden_states = residual + hidden_states
+        residual = hidden_states
+
+        # Norm
+        hidden_states = self.layer_norm2(hidden_states)
+        # FeedForward
+        hidden_states = self.mlp(hidden_states)
+
+        # Add
+        hidden_states = residual + hidden_states
+
+        # [batch_size, num_patches, embed_size]
+        return hidden_states
+
+
+class TransformerEncoder(nn.Module):
+    """Stack multiple encoder layers.
+
+    Reference:
+        Vit: https://arxiv.org/pdf/2010.11929
+    """
+
+    def __init__(self, config: VisionConfig):
+        super().__init__()
+        self.config = config
+        self.layers = nn.ModuleList(
+            [EncoderLayer(config) for _ in range(config.num_hidden_layers)]
+        )
+
+    def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
+        """Run stacked encoder"""
+        for encoder_layer in self.layers:
+            hidden_states = encoder_layer(hidden_states)
+        return hidden_states
 
 
 class VisionEmbeddings(nn.Module):
@@ -101,79 +242,6 @@ class VisionEmbeddings(nn.Module):
         return embeddings
 
 
-class MHAttention(nn.Module):
-    """Multi-Head Attention Layer.
-    
-    Reference:
-        Transformer: https://arxiv.org/pdf/1706.03762
-    """
-
-
-class MLP(nn.Module):
-    """Feed Forward Layer of Transformer Encoder.
-
-    Reference:
-        Transformer: https://arxiv.org/pdf/1706.03762
-    """
-
-    def __init__(self, config: VisionConfig):
-        super().__init__()
-        self.config = config
-        self.fc1 = nn.Linear(config.hidden_size, config.feedforward_size)
-        self.fc2 = nn.Linear(config.feedforward_size, config.hidden_size)
-
-    def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
-        """Position-wise fully connected feed-forward network"""
-        # [batch_size, num_patches, embed_size]
-        hidden_states = self.fc1(hidden_states)
-        # [batch_size, num_patches, embed_size * 4]
-        hidden_states = F.gelu(hidden_states, approximate="tanh")
-        # [batch_size, num_patches, embed_size]
-        hidden_states = self.fc2(hidden_states)
-        return hidden_states
-
-
-class EncoderLayer(nn.Module):
-    """Encoder layer of transformer.
-
-    Reference:
-        Vit: https://arxiv.org/pdf/2010.11929
-    """
-
-    def __init__(self, config: VisionConfig):
-        super().__init__()
-        self.embed_dim = config.hidden_size
-        self.layer_norm1 = nn.LayerNorm(self.embed_dim, eps=config.layer_norm_eps)
-        self.self_attn = MHAttention(config)
-        self.layer_norm2 = nn.LayerNorm(self.embed_dim, eps=config.layer_norm_eps)
-        self.mlp = MLP(config)
-
-    def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
-        """Standard transformer encoder layer"""
-        # [batch_size, num_patches, embed_size]
-        residual = hidden_states
-
-        # Norm
-        hidden_states = self.layer_norm1(hidden_states)
-        # Multi-Head Attention
-        hidden_states, _ = self.self_attn(hidden_states)
-
-        # Add
-        hidden_states = residual + hidden_states
-        residual = hidden_states
-
-        # Norm
-        hidden_states = self.layer_norm2(hidden_states)
-        # FeedForward
-        hidden_states = self.mlp(hidden_states)
-
-        # Add
-        hidden_states = residual + hidden_states
-
-        # [batch_size, num_patches, embed_size]
-        return hidden_states
-
-
 class VisionTransformer(nn.Module):
     """Vision Transformer used for extraction of feature representations.
 
@@ -191,7 +259,7 @@ class VisionTransformer(nn.Module):
     def forward(self, imgs: torch.Tensor) -> torch.Tensor:
         """Transform images into number of patches"""
         hidden_states = self.embeddings(imgs)
-        last_hidden_state = self.encoder(input_embeds=hidden_states)
+        last_hidden_state = self.encoder(hidden_states)
         last_hidden_state = self.layernorm(last_hidden_state)
         return last_hidden_state
 
